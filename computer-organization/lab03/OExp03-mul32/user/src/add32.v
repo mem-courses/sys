1aa1a1b05aa8113cb5a8945cee3406e9a8705085
memset0 <memset0@outlook.com>
2024-10-03 16:07:18 +0800

    lab03: finish mul32

---
+++ computer-organization/lab03/OExp03-mul32/user/src/add32.v
@@ -0,0 +1,10 @@
+module add32 (
+   input [31:0] a,
+   input [31:0] b,
+   output c,
+   output [31:0] s
+);
+
+   assign {c, s} = {1'b0, a} + {1'b0, b};
+
+endmodule
--- computer-organization/lab03/OExp03-mul32/user/src/mul32.v
+++ computer-organization/lab03/OExp03-mul32/user/src/mul32.v
@@ -11,34 +11,42 @@ module mul32 (
 );
 
    reg [5:0] count;
-   reg carry;
    reg calculating;
+
+   wire carry;
+   wire [31:0] sum;
+
+   add32 add32_v (
+      .a(product[63:32]),
+      .b(multiplicand),
+      .c(carry),
+      .s(sum)
+   );
 
    always @(posedge clk or posedge rst) begin
       if (rst) begin
          // reset all
-         product <= 64'b0;
-         count <= 6'b0;
-         finish <= 1'b0;
+         product     <= 64'b0;
+         count       <= 6'b0;
+         finish      <= 1'b0;
          calculating <= 1'b0;
       end else begin
          // calculate
          if (start && !calculating) begin
-            product <= {32'b0, multiplier};
-            count <= 6'b0;
-            finish <= 1'b0;
+            product     <= {32'b0, multiplier};
+            count       <= 6'b0;
+            finish      <= 1'b0;
             calculating <= 1'b1;
          end else if (calculating) begin
             if (count < 32) begin
                if (product[0]) begin
-                  {carry, product[63:32]} <= {1'b0, product[63:32]} + {1'b0, multiplicand};
+                  product <= {carry, sum, product[31:1]};
                end else begin
-                  carry <= 1'b0;
+                  product <= {1'b0, product[63:1]};
                end
-               product <= {carry, product[63:1]};
-               count   <= count + 1;
+               count <= count + 1;
             end else begin
-               finish <= 1'b1;
+               finish      <= 1'b1;
                calculating <= 1'b0;
             end
          end
