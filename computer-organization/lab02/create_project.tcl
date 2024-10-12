b4b28e3a8d61304890bdc559a32551b96e3616de
memset0 <memset0@outlook.com>
2024-10-12 18:03:54 +0800

    lab02: move IP core to public folder

--- computer-organization/lab02/create_project.tcl
+++ computer-organization/lab02/create_project.tcl
@@ -1,21 +1,28 @@
-# vivado -mode tcl -source create_project.tcl
+# run: vivado -mode tcl -source create_project.tcl
 
 set project_name "lab2"
 set target_dir "./prj"
 
+# set project parameters
 set_param general.maxThreads 16
 
+# create project
 create_project -force $project_name $target_dir -part xc7a100tcsg324-1
 
-add_files -scan_for_includes -fileset sources_1 ./user/src
+# add source files
+set source_dirs [list "./user/src" "../public/ip"]
+foreach source_dir $source_dirs {
+  add_files -scan_for_includes -fileset sources_1 $source_dir
+  foreach edf_file [glob -nocomplain -directory $source_dir *.edf] {
+      read_edif $edf_file
+  }
+}
+
+# add sim files and constraints
 add_files -scan_for_includes -fileset sim_1 ./user/sim
 add_files -scan_for_includes -fileset constrs_1 ./user/data
 
-set edf_files [glob -nocomplain -directory "./user/src" *.edf]
-foreach edf_file $edf_files {
-    read_edif $edf_file
-}
-
+# create ROM_D with I_mem.coe
 create_ip -name dist_mem_gen -vendor xilinx.com -library ip -version 8.0 -module_name ROM_D
 set_property -dict [list \
   CONFIG.coefficient_file [file normalize ./user/data/I_mem.coe] \
