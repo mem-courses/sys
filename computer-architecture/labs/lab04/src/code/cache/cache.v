db1033cf5805a9a01c38d0c1ecc9fbfd53ccb447
memset0 <memset0@outlook.com>
2025-04-22 14:06:46 +0800

    lab04: 完成代码

--- computer-architecture/labs/lab04/src/code/cache/cache.v
+++ computer-architecture/labs/lab04/src/code/cache/cache.v
@@ -3,25 +3,157 @@
 // | tag 23 | index 5 | word 2 | byte 2 |
 
 module cache (
-	input wire clk,  // clock
-	input wire rst,  // reset
-	input wire [ADDR_BITS-1:0] addr,  // address
-    input wire load,    //  read refreshes recent bit
-	input wire store,  // set valid to 1 and reset dirty to 0
-	input wire edit,  // set dirty to 1
-	input wire invalid,  // reset valid to 0
-    input wire [2:0] u_b_h_w, // select signed or not & data width
-                              // please refer to definition of LB, LH, LW, LBU, LHU in RV32I Instruction Set  
-	input wire [31:0] din,  // data write in
-	output reg hit = 0,  // hit or not
-	output reg [31:0] dout = 0,  // data read out
-	output reg valid = 0,  // valid bit
-	output reg dirty = 0,  // dirty bit
-	output reg [TAG_BITS-1:0] tag = 0  // tag bits
-	);
+   input  wire                 clk,        // clock
+   input  wire                 rst,        // reset
+   input  wire [ADDR_BITS-1:0] addr,       // address
+   input  wire                 load,       //  read refreshes recent bit
+   input  wire                 store,      // set valid to 1 and reset dirty to 0
+   input  wire                 edit,       // set dirty to 1
+   input  wire                 invalid,    // reset valid to 0
+   input  wire [          2:0] u_b_h_w,    // select signed or not & data width: 读数据的位宽
+                                           // please refer to definition of LB, LH, LW, LBU, LHU in RV32I Instruction Set  
+   input  wire [         31:0] din,        // data write in: 用于写入缓存的数据
+   output reg                  hit = 0,    // hit or not: 输出是否命中
+   output reg  [         31:0] dout = 0,   // data read out: 从缓存中读出的数据
+   output reg                  valid = 0,  // valid bit
+   output reg                  dirty = 0,  // dirty bit
+   output reg  [ TAG_BITS-1:0] tag = 0     // tag bits
+);
 
-    `include "addr_define.vh"
+   `include "addr_define.vh"
 
-    // your implementation
+   wire [31:0] word1, word2;
+   wire [15:0] half_word1, half_word2;
+   wire [7:0] byte1, byte2;
+   wire recent1, recent2, valid1, valid2, dirty1, dirty2;
+   wire [TAG_BITS-1:0] tag1, tag2;
+   wire hit1, hit2;
+
+   reg [ELEMENT_NUM-1:0] inner_recent = 0;  // 每个缓冲块的最近使用位（用于LRU策略）
+   reg [ELEMENT_NUM-1:0] inner_valid = 0;  // 每个缓冲块的有效位
+   reg [ELEMENT_NUM-1:0] inner_dirty = 0;  // 每个缓冲块的脏位
+   reg [TAG_BITS-1:0] inner_tag[0:ELEMENT_NUM-1];  // 每个缓存行的标记
+   // 64 elements, 2 ways set associative => 32 sets
+   reg [31:0] inner_data[0:ELEMENT_NUM*ELEMENT_WORDS-1];  // 缓存中的实际数据内容，用于在缓存命中时提供数据
+
+   // initialize tag and data with 0
+   integer i;
+   initial begin
+      for (i = 0; i < ELEMENT_NUM; i = i + 1) inner_tag[i] = 23'b0;
+
+      for (i = 0; i < ELEMENT_NUM * ELEMENT_WORDS; i = i + 1) inner_data[i] = 32'b0;
+   end
+
+   // the bits in an input address:
+   wire [TAG_BITS-1:0] addr_tag;
+   wire [SET_INDEX_WIDTH-1:0] addr_index;     // idx of set
+   wire [ELEMENT_INDEX_WIDTH-1:0] addr_element1;
+   wire [ELEMENT_INDEX_WIDTH-1:0] addr_element2;     // idx of element
+   wire [ELEMENT_INDEX_WIDTH+ELEMENT_WORDS_WIDTH-1:0] addr_word1;
+   wire [ELEMENT_INDEX_WIDTH+ELEMENT_WORDS_WIDTH-1:0] addr_word2; // element index + word index
+
+   // 地址解析
+   assign addr_tag      = addr[ADDR_BITS-1:ADDR_BITS-TAG_BITS];  //need to fill in
+   assign addr_index    = addr[ADDR_BITS-TAG_BITS-1:ADDR_BITS-TAG_BITS-SET_INDEX_WIDTH];  //need to fill in
+   assign addr_element1 = {addr_index, 1'b0};
+   assign addr_element2 = {addr_index, 1'b1};  //need to fill in
+   assign addr_word1    = {addr_element1, addr[ELEMENT_WORDS_WIDTH+WORD_BYTES_WIDTH-1:WORD_BYTES_WIDTH]};
+   assign addr_word2    = {addr_element2, addr[ELEMENT_WORDS_WIDTH+WORD_BYTES_WIDTH-1:WORD_BYTES_WIDTH]};  //need to fill in
+
+   //  数据解析
+   assign word1         = inner_data[addr_word1];
+   assign word2         = inner_data[addr_word2];  //need to fill in
+   assign half_word1    = addr[1] ? word1[31:16] : word1[15:0];
+   assign half_word2    = addr[1] ? word2[31:16] : word2[15:0];  //need to fill in
+   assign byte1         = addr[1] ? (addr[0] ? word1[31:24] : word1[23:16]) : (addr[0] ? word1[15:8] : word1[7:0]);
+   assign byte2         = addr[1] ? (addr[0] ? word2[31:24] : word2[23:16]) : (addr[0] ? word2[15:8] : word2[7:0]);  //need to fill in
+
+   // 缓存状态解析
+   assign recent1       = inner_recent[addr_element1];
+   assign recent2       = inner_recent[addr_element2];  //need to fill in
+   assign valid1        = inner_valid[addr_element1];
+   assign valid2        = inner_valid[addr_element2];  //need to fill in
+   assign dirty1        = inner_dirty[addr_element1];
+   assign dirty2        = inner_dirty[addr_element2];  //need to fill in
+   assign tag1          = inner_tag[addr_element1];
+   assign tag2          = inner_tag[addr_element2];  //need to fill in
+   assign hit1          = valid1 & (tag1 == addr_tag);
+   assign hit2          = valid2 & (tag2 == addr_tag);  //need to fill in
+
+   always @(posedge clk) begin
+      valid <= recent1 ? valid2 : valid1;  //need to fill in
+      dirty <= recent1 ? dirty2 : dirty1;  //need to fill in
+      tag   <= recent1 ? tag2 : tag1;  //need to fill in
+      hit   <= hit1 | hit2;  //need to fill in
+
+      // read $ with load==0 means moving data from $ to mem
+      // no need to update recent bit
+      // otherwise the refresh process will be affected
+      if (load) begin
+         if (hit1) begin
+            dout <= u_b_h_w[1] ? word1 : u_b_h_w[0] ? {u_b_h_w[2] ? 16'b0 : {16{half_word1[15]}}, half_word1} : {u_b_h_w[2] ? 24'b0 : {24{byte1[7]}}, byte1};
+            // inner_recent will be refreshed only on r/w hit
+            // (including the r/w hit after miss and replacement)
+            inner_recent[addr_element1] <= 1'b1;
+            inner_recent[addr_element2] <= 1'b0;
+         end else if (hit2) begin
+            dout <= u_b_h_w[1] ? word2 : u_b_h_w[0] ? {u_b_h_w[2] ? 16'b0 : {16{half_word2[15]}}, half_word2} : {u_b_h_w[2] ? 24'b0 : {24{byte2[7]}}, byte2};
+            inner_recent[addr_element1] <= 1'b0;
+            inner_recent[addr_element2] <= 1'b1;
+            //need to fill in
+         end
+      end else dout <= inner_data[recent1?addr_word2 : addr_word1];
+
+      if (edit) begin
+         if (hit1) begin
+            inner_data[addr_word1] <=  u_b_h_w[1] ?  din : u_b_h_w[0] ?addr[1] ? {din[15:0], word1[15:0]} : {word1[31:16], din[15:0]} : addr[1] ? addr[0] ? {din[7:0], word1[23:0]}  : {word1[31:24], din[7:0], word1[15:0]}  : addr[0] ? {word1[31:16], din[7:0], word1[7:0]}        : {word1[31:8], din[7:0]}     ;
+            inner_dirty[addr_element1] <= 1'b1;
+            inner_recent[addr_element1] <= 1'b1;
+            inner_recent[addr_element2] <= 1'b0;
+         end else if (hit2) begin
+            inner_data[addr_word2] <=
+                    u_b_h_w[1] ? din :
+                    u_b_h_w[0] ? addr[1] ? {din[15:0], word2[15:0]} : {word2[31:16],
+                    din[15:0]} :
+                    addr[1] ?
+                    addr[0] ? {din[7:0], word2[23:0]} :
+                    {word2[31:24], din[7:0], word2[15:0]} :
+                    addr[0] ?
+                    {word2[31:16], din[7:0], word2[7:0]} :
+                    {word2[31:8], din[7:0]};
+            inner_dirty[addr_element2] <= 1'b1;
+            inner_recent[addr_element1] <= 1'b0;
+            inner_recent[addr_element2] <= 1'b1;
+            //need to fill in
+         end
+      end
+
+      if (store) begin
+         if (recent1) begin  // replace 2
+            inner_data[addr_word2] <= din;
+            inner_valid[addr_element2] <= 1'b1;
+            inner_dirty[addr_element2] <= 1'b0;
+            inner_tag[addr_element2] <= addr_tag;
+         end else begin
+            // recent2 == 1 => replace 1
+            // recent2 == 0 => no data in this set, place to 1
+            inner_data[addr_word1] <= din;
+            inner_valid[addr_element1] <= 1'b1;
+            inner_dirty[addr_element1] <= 1'b0;
+            inner_tag[addr_element1] <= addr_tag;
+            //need to fill in
+         end
+      end
+
+      // not used currently, can be used to reset the cache.
+      if (invalid) begin
+         inner_recent[addr_element1] <= 1'b0;
+         inner_recent[addr_element2] <= 1'b0;
+         inner_valid[addr_element1]  <= 1'b0;
+         inner_valid[addr_element2]  <= 1'b0;
+         inner_dirty[addr_element1]  <= 1'b0;
+         inner_dirty[addr_element2]  <= 1'b0;
+      end
+   end
 
 endmodule
--- computer-architecture/labs/lab04/src/code/cache/cmu.v
+++ computer-architecture/labs/lab04/src/code/cache/cmu.v
@@ -1,194 +1,177 @@
 module cmu (
-        // CPU side
-		input clk,
-		input rst,
-		input [31:0] addr_rw,
-		input en_r,
-		input en_w,
-        input [2:0] u_b_h_w,
-		input [31:0] data_w,
-		output [31:0] data_r,
-		output stall,
+   // CPU side
+   input         clk,
+   input         rst,
+   input  [31:0] addr_rw,
+   input         en_r,
+   input         en_w,
+   input  [ 2:0] u_b_h_w,
+   input  [31:0] data_w,
+   output [31:0] data_r,
+   output        stall,
 
-        // mem side
-		output reg mem_cs_o = 0,
-		output reg mem_we_o = 0,
-		output reg [31:0] mem_addr_o = 0,
-		input [31:0] mem_data_i,
-		output [31:0] mem_data_o,
-		input mem_ack_i,
+   // mem side
+   output reg        mem_cs_o = 0,
+   output reg        mem_we_o = 0,
+   output reg [31:0] mem_addr_o = 0,
+   input      [31:0] mem_data_i,
+   output     [31:0] mem_data_o,
+   input             mem_ack_i,       // 用来判断内存的读写操作是否已经完成
 
-        // debug info
-        output [2:0] cmu_state
-	);
+   // debug info
+   output [2:0] cmu_state
+);
 
-    `include "addr_define.vh"
+   `include "addr_define.vh"
 
-    reg [ADDR_BITS-1:0] cache_addr = 0;
-    reg cache_load = 0;
-    reg cache_store = 0;
-    reg cache_edit = 0;
-    reg [2:0] cache_u_b_h_w = 0;
-    reg [WORD_BITS-1:0] cache_din = 0;
-    wire cache_hit;
-    wire [WORD_BITS-1:0] cache_dout;
-    wire cache_valid;
-    wire cache_dirty;
-    wire [TAG_BITS-1:0] cache_tag;
+   reg [ADDR_BITS-1:0] cache_addr = 0;
+   reg cache_load = 0;
+   reg cache_store = 0;
+   reg cache_edit = 0;
+   reg [2:0] cache_u_b_h_w = 0;
+   reg [WORD_BITS-1:0] cache_din = 0;
+   wire cache_hit;
+   wire [WORD_BITS-1:0] cache_dout;
+   wire cache_valid;
+   wire cache_dirty;
+   wire [TAG_BITS-1:0] cache_tag;
 
-    cache CACHE (
-        .clk(~clk),
-        .rst(rst),
-        .addr(cache_addr),
-        .load(cache_load),
-        .store(cache_store),
-        .edit(cache_edit),
-        .invalid(1'b0),
-        .u_b_h_w(cache_u_b_h_w),
-        .din(cache_din),
-        .hit(cache_hit),
-        .dout(cache_dout),
-        .valid(cache_valid),
-        .dirty(cache_dirty),
-        .tag(cache_tag)
-    );
+   cache CACHE (
+      .clk    (~clk),
+      .rst    (rst),
+      .addr   (cache_addr),
+      .load   (cache_load),
+      .store  (cache_store),
+      .edit   (cache_edit),
+      .invalid(1'b0),
+      .u_b_h_w(cache_u_b_h_w),
+      .din    (cache_din),
+      .hit    (cache_hit),
+      .dout   (cache_dout),
+      .valid  (cache_valid),
+      .dirty  (cache_dirty),
+      .tag    (cache_tag)
+   );
 
-    localparam
-        S_IDLE = 0,
-        S_PRE_BACK = 1,
-        S_BACK = 2,
-        S_FILL = 3,
-        S_WAIT = 4;
+   localparam S_IDLE = 0, S_PRE_BACK = 1, S_BACK = 2, S_FILL = 3, S_WAIT = 4;
 
-    reg [2:0]state = 0;
-    reg [2:0]next_state = 0;
-    reg [ELEMENT_WORDS_WIDTH-1:0]word_count = 0;
-    reg [ELEMENT_WORDS_WIDTH-1:0]next_word_count = 0;
-    assign cmu_state = state;
+   reg [2:0] state = 0;
+   reg [2:0] next_state = 0;
+   reg [ELEMENT_WORDS_WIDTH-1:0] word_count = 0;
+   reg [ELEMENT_WORDS_WIDTH-1:0] next_word_count = 0;
+   assign cmu_state = state;
 
-    always @ (posedge clk) begin
-        if (rst) begin
-            state <= S_IDLE;
-            word_count <= 2'b00;
-        end
-        else begin
-            state <= next_state;
-            word_count <= next_word_count;
-        end
-    end
+   always @(posedge clk) begin
+      if (rst) begin
+         state <= S_IDLE;
+         word_count <= 2'b00;
+      end else begin
+         state <= next_state;
+         word_count <= next_word_count;
+      end
+   end
 
-    // state ctrl
-    always @ (*) begin
-        if (rst) begin
-            next_state = S_IDLE;
-            next_word_count = 2'b00;
-        end
-        else begin
-            case (state)
-                S_IDLE: begin
-                    if (en_r || en_w) begin
-                        if (cache_hit)
-                            next_state = ??;
-                        else if (cache_valid && cache_dirty)
-                            next_state = ??;
-                        else
-                            next_state = ??;
-                    end
-                    next_word_count = 2'b00;
-                end
-
-                S_PRE_BACK: begin
-                    next_state = ??;
-                    next_word_count = 2'b00;
-                end
-
-                S_BACK: begin
-                    if (mem_ack_i && word_count == {ELEMENT_WORDS_WIDTH{1'b1}})    // 2'b11 in default case
-                        next_state = ??;
-                    else
-                        next_state = ??;
-
-                    if (mem_ack_i)
-                        next_word_count = ??;
-                    else
-                        next_word_count = word_count;
-                end
-
-                S_FILL: begin
-                    if (mem_ack_i && word_count == {ELEMENT_WORDS_WIDTH{1'b1}})
-                        next_state = ??;
-                    else
-                        next_state = ??;
-
-                    if (mem_ack_i)
-                        next_word_count = ??;
-                    else
-                        next_word_count = word_count;
-                end
-
-                S_WAIT: begin
-                    next_state = ??;
-                    next_word_count = 2'b00;
-                end
-            endcase
-        end
-    end
-    
-    // cache ctrl
-    always @ (*) begin
-        case(state)
-            S_IDLE, S_WAIT: begin
-                cache_addr = addr_rw;
-                cache_load = en_r;
-                cache_edit = en_w;
-                cache_store = 1'b0;
-                cache_u_b_h_w = u_b_h_w;
-                cache_din = data_w;
+   // state ctrl
+   always @(*) begin
+      if (rst) begin
+         next_state = S_IDLE;
+         next_word_count = 2'b00;
+      end else begin
+         case (state)
+            S_IDLE: begin
+               if (en_r || en_w) begin
+                  if (cache_hit) next_state = S_IDLE;  // 如果cache hit，说明CMU无需额外操作，继续保持在IDLE状态
+                  else if (cache_valid && cache_dirty) next_state = S_PRE_BACK;  // 如果cache miss & dirty，说明需要先写回主存再取出，进入PRE_BACK状态
+                  else next_state = S_FILL;  // 如果cache miss & !dirty，说明直接从内存中取出即可
+               end
+               next_word_count = 2'b00;
             end
-            S_BACK, S_PRE_BACK: begin
-                cache_addr = {addr_rw[ADDR_BITS-1:BLOCK_WIDTH], next_word_count, {ELEMENT_WORDS_WIDTH{1'b0}}};
-                cache_load = 1'b0;
-                cache_edit = 1'b0;
-                cache_store = 1'b0;
-                cache_u_b_h_w = 3'b010;
-                cache_din = 32'b0;
-            end
-            S_FILL: begin
-                cache_addr = {addr_rw[ADDR_BITS-1:BLOCK_WIDTH], word_count, {ELEMENT_WORDS_WIDTH{1'b0}}};
-                cache_load = 1'b0;
-                cache_edit = 1'b0;
-                cache_store = mem_ack_i;
-                cache_u_b_h_w = 3'b010;
-                cache_din = mem_data_i;
-            end
-        endcase
-    end
-    assign data_r = cache_dout;
 
-    // mem ctrl
-    always @ (*) begin
-        case (next_state)
-            S_IDLE, S_PRE_BACK, S_WAIT: begin
-                mem_cs_o = 1'b0;
-                mem_we_o = 1'b0;
-                mem_addr_o = 32'b0;
+            S_PRE_BACK: begin
+               next_state = S_BACK;
+               next_word_count = 2'b00;
             end
 
             S_BACK: begin
-                mem_cs_o = 1'b1;
-                mem_we_o = 1'b1;
-                mem_addr_o = {cache_tag, addr_rw[ADDR_BITS-TAG_BITS-1:BLOCK_WIDTH], next_word_count, {ELEMENT_WORDS_WIDTH{1'b0}}};
+               if (mem_ack_i && word_count == {ELEMENT_WORDS_WIDTH{1'b1}})  // 2'b11 in default case
+                  next_state = S_FILL;
+               else next_state = S_BACK;
+
+               if (mem_ack_i) next_word_count = word_count + 1;
+               else next_word_count = word_count;
             end
 
             S_FILL: begin
-                mem_cs_o = 1'b1;
-                mem_we_o = 1'b0;
-                mem_addr_o = {addr_rw[ADDR_BITS-1:BLOCK_WIDTH], next_word_count, {ELEMENT_WORDS_WIDTH{1'b0}}};
-            end
-        endcase
-    end
-    assign mem_data_o = cache_dout;
+               if (mem_ack_i && word_count == {ELEMENT_WORDS_WIDTH{1'b1}}) next_state = S_FILL;
+               else next_state = S_BACK;
 
-    assign stall = ??;
+               if (mem_ack_i) next_word_count = word_count + 1;
+               else next_word_count = word_count;
+            end
+
+            S_WAIT: begin
+               next_state = S_IDLE;
+               next_word_count = 2'b00;
+            end
+         endcase
+      end
+   end
+
+   // cache ctrl
+   always @(*) begin
+      case (state)
+         S_IDLE, S_WAIT: begin
+            cache_addr = addr_rw;
+            cache_load = en_r;
+            cache_edit = en_w;
+            cache_store = 1'b0;
+            cache_u_b_h_w = u_b_h_w;
+            cache_din = data_w;
+         end
+         S_BACK, S_PRE_BACK: begin
+            cache_addr = {addr_rw[ADDR_BITS-1:BLOCK_WIDTH], next_word_count, {ELEMENT_WORDS_WIDTH{1'b0}}};
+            cache_load = 1'b0;
+            cache_edit = 1'b0;
+            cache_store = 1'b0;
+            cache_u_b_h_w = 3'b010;
+            cache_din = 32'b0;
+         end
+         S_FILL: begin
+            cache_addr = {addr_rw[ADDR_BITS-1:BLOCK_WIDTH], word_count, {ELEMENT_WORDS_WIDTH{1'b0}}};
+            cache_load = 1'b0;
+            cache_edit = 1'b0;
+            cache_store = mem_ack_i;
+            cache_u_b_h_w = 3'b010;
+            cache_din = mem_data_i;
+         end
+      endcase
+   end
+   assign data_r = cache_dout;
+
+   // mem ctrl
+   always @(*) begin
+      case (next_state)
+         S_IDLE, S_PRE_BACK, S_WAIT: begin
+            mem_cs_o   = 1'b0;
+            mem_we_o   = 1'b0;
+            mem_addr_o = 32'b0;
+         end
+
+         S_BACK: begin
+            mem_cs_o   = 1'b1;
+            mem_we_o   = 1'b1;
+            mem_addr_o = {cache_tag, addr_rw[ADDR_BITS-TAG_BITS-1:BLOCK_WIDTH], next_word_count, {ELEMENT_WORDS_WIDTH{1'b0}}};
+         end
+
+         S_FILL: begin
+            mem_cs_o   = 1'b1;
+            mem_we_o   = 1'b0;
+            mem_addr_o = {addr_rw[ADDR_BITS-1:BLOCK_WIDTH], next_word_count, {ELEMENT_WORDS_WIDTH{1'b0}}};
+         end
+      endcase
+   end
+   assign mem_data_o = cache_dout;
+
+   assign stall = next_state != S_IDLE;  // 要求CPU stall的输出信号
 
 endmodule
